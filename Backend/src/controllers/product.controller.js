86ab9cc0c90a78c11f6eaac1053882f68373e295
Akshat Sahay <akshatsahay2106@gmail.com>
2026-06-12 15:06:01 +0530

    Refactor code structure for improved readability and maintainability

--- Backend/src/controllers/product.controller.js
+++ Backend/src/controllers/product.controller.js
@@ -33,3 +33,15 @@ res.status(201).json({
 
     })
 }
+
+export async function getSellerProducts (req, res) {
+    const seller = req.user;
+
+    const products = await productModel.find({seller: seller._id});
+
+    res.status(200).json({
+        message: "Products fetched successfully",
+        success: true,
+        products
+    })
+}
